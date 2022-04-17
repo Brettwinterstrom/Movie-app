6e7a8e5d0ac34ed78581edc581c62bb5ccf911b7
Brett Winterstrom <brettwinterstrom@yahoo.com>
2022-04-17 13:27:08 -0700

    Made header component and added React Router capabilities

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,37 +1,22 @@
-import { useState } from "react";
-import MovieList from "./components/MovieList";
+import "./index.css";
+import Header from "./components/Header";
+import WatchList from "./components/Watchlist";
+import Watched from "./components/Watched";
+import Add from "./components/Add";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Star Wars: Episode I - The Phantom Menace",
-      Year: "1999",
-      imdbID: "tt0120915",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode II - Attack of the Clones",
-      Year: "2002",
-      imdbID: "tt0121765",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode III - Revenge of the Sith",
-      Year: "2005",
-      imdbID: "tt0121766",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg",
-    },
-  ]);
-
   return (
     <>
-      <MovieList movies={movies} />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<WatchList />} />
+
+          <Route path="/watched" element={<Watched />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ movie-app/src/components/Watched.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Watched = () => {
+  return (
+    <>
+      <h1>Watched Page</h1>
+    </>
+  );
+};
+
+export default Watched;
